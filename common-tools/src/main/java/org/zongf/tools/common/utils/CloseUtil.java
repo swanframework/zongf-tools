9ca130073e38749fcf1b2233d1a9a896007655e4
zonggaofeng <zonggaofeng@autohome.com.cn>
2020-04-17 13:35:15 +0800

    优化工具方法

--- common-tools/src/main/java/org/zongf/tools/common/utils/CloseUtil.java
+++ common-tools/src/main/java/org/zongf/tools/common/utils/CloseUtil.java
@@ -1,5 +1,7 @@
 package org.zongf.tools.common.utils;
 
+import org.slf4j.Logger;
+import org.zongf.tools.log.util.LogUtil;
 
 /** 流关闭工具类
  * @author: zongf
@@ -7,19 +9,21 @@ package org.zongf.tools.common.utils;
  */
 public class CloseUtil {
 
+    private static Logger log = LogUtil.getLogger(CloseUtil.class);
+
     /**手工关闭流
      * @param closeables 任意多个实现AutoCloseable接口的对象
      * @author zongf
      * @date 2019-07-01
      */
     public static void close(AutoCloseable... closeables) {
-        if (closeables != null) {
-            for (AutoCloseable closeable: closeables) {
-                try {
+        for (AutoCloseable closeable: closeables) {
+            try {
+                if (closeable != null) {
                     closeable.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
                 }
+            } catch (Exception e) {
+                log.warn("流关闭异常, 异常原因:{}", e.getMessage());
             }
         }
     }
