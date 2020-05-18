3f740c900ea8b3736dea11c7a2e13abab7242fa7
zonggaofeng <zonggaofeng@autohome.com.cn>
2020-05-18 19:53:22 +0800

    更名为LockerConfig

--- zongf-test-tools/src/main/java/org/zongf/test/tools/config/LockerConfig.java
+++ zongf-test-tools/src/main/java/org/zongf/test/tools/config/LockerConfig.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zongf.tools.spring.lock.RedisLocker;
 
-/** Redis 配置
+/** 锁 配置
  * @author zongf
  * @date 2020-05-16
  */
 @Configuration
-public class RedisConfig {
+public class LockerConfig {
 
     // 目前springboot不能扫描jar包中的组件,因此需要手工注入
     @Bean
