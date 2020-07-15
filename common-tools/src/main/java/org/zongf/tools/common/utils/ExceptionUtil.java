8f723220fd2d671b8288b81e1449bd3a9bd2c238
zonggaofeng <zonggaofeng@autohome.com.cn>
2020-07-15 15:38:39 +0800

    新增异常工具类

---
+++ common-tools/src/main/java/org/zongf/tools/common/utils/ExceptionUtil.java
@@ -0,0 +1,34 @@
+package org.zongf.tools.common.utils;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/** 异常工具类
+ * @author zongf
+ * @date 2020-07-15
+ */
+public final class ExceptionUtil {
+
+    /** 解析异常堆栈信息
+     * @param ex
+     * @return String
+     * @author zongf
+     * @date 2020-07-15
+     */
+    public static String parseStackTrace(Exception ex) {
+        StringWriter sw = null;
+        PrintWriter pw = null;
+
+        try {
+            sw = new StringWriter();
+            pw = new PrintWriter(sw);
+            ex.printStackTrace(pw);
+            return sw.toString();
+        } catch (Exception e) {
+            return ex.getMessage();
+        } finally {
+            CloseUtil.close(sw, pw);
+        }
+    }
+
+}
